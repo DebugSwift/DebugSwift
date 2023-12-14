ba917569db549820562022f2ab9bab53887214a8
Matheus Gois <matheus.gois@btgpactual.com>
2023-12-14 20:47:03 -0300

    [Main] - New Updates ✨

--- NewCocoaDebug/AppDelegate.swift
+++ NewCocoaDebug/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         self.window = UIWindow(frame: UIScreen.main.bounds)
-        let navigation = UINavigationController(rootViewController: TabBarController())
+        let navigation = UINavigationController(rootViewController: ViewController())
         self.window?.rootViewController = navigation
         self.window?.makeKeyAndVisible()
 
--- NewCocoaDebug/TabBar/TabBarController.swift
+++ NewCocoaDebug/TabBar/TabBarController.swift
@@ -27,6 +27,14 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupTabBar()
+        setupNavigation()
+    }
+
+}
+
+fileprivate extension TabBarController {
+    func setupTabBar() {
         let controllers = [
             NetworkViewController(),
             PerformanceViewController(),
@@ -42,6 +50,29 @@ class TabBarController: UITabBarController {
         tabBar.tintColor = .white
         tabBar.backgroundColor = .black
         tabBar.unselectedItemTintColor = .gray
+    }
+
+    func setupNavigation() {
+        // Remove the default back button
+        navigationItem.hidesBackButton = true
+
+        let closeButton: UIBarButtonItem
+        if #available(iOS 14.0, *) {
+            closeButton = UIBarButtonItem(systemItem: .close)
+            closeButton.target = self
+            closeButton.action = #selector(closeButtonTapped)
+        } else {
+            // Add a custom "close" button
+            closeButton = UIBarButtonItem(title: "Close", style: .plain, target: self, action: #selector(closeButtonTapped))
+        }
+
+        navigationItem.rightBarButtonItem = closeButton
+    }
+
+    @objc func closeButtonTapped() {
+        // Handle close button tap
+        // For example, you can dismiss the current view controller
+        navigationController?.popViewController(animated: true)
     }
 }
 
