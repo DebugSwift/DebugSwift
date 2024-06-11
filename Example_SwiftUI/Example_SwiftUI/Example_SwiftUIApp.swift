2334c0f7cfaff4d240a893f534d5dfb75c454f82
Emil Atanasov <heitara@gmail.com>
2024-06-12 01:38:32 +0300

    [Bugfix] Fix uploadProgress with Alamofire to the Example (#76)
    Co-authored-by: Matheus Gois <matheus.gois@btgpactual.com>

--- Example_SwiftUI/Example_SwiftUI/Example_SwiftUIApp.swift
+++ Example_SwiftUI/Example_SwiftUI/Example_SwiftUIApp.swift
@@ -7,6 +7,7 @@
 
 import DebugSwift
 import SwiftUI
+import Alamofire
 
 @available(iOS 14.0, *)
 @main
@@ -29,9 +30,8 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         // DebugSwift.setup(hideFeatures: [.interface, .app, .resources, .performance])
         DebugSwift.setup()
         DebugSwift.show()
-        DebugSwift.App.customControllers = {
-            self.additionalViewControllers()
-        }
+
+        DebugSwift.Network.delegate = self
         return true
     }
 
@@ -39,5 +39,32 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         let viewController = UITableViewController()
         viewController.title = "PURE"
         return [viewController]
+    }
+}
+
+// MARK: - Alamofire bugfix in uploadProgress
+
+extension AppDelegate: CustomHTTPProtocolDelegate {
+    func urlSession(
+        _ protocol: URLProtocol,
+        _ session: URLSession,
+        task: URLSessionTask,
+        didSendBodyData bytesSent: Int64,
+        totalBytesSent: Int64,
+        totalBytesExpectedToSend: Int64
+    ) {
+
+        Session.default.session.getAllTasks { tasks in
+            let uploadTask = tasks.first(where: { $0.taskIdentifier == task.taskIdentifier }) ?? task
+            Session.default.rootQueue.async {
+                Session.default.delegate.urlSession(
+                    session,
+                    task: uploadTask,
+                    didSendBodyData: bytesSent,
+                    totalBytesSent: totalBytesSent,
+                    totalBytesExpectedToSend: totalBytesExpectedToSend
+                )
+            }
+        }
     }
 }
