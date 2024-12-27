930f0a3ade1573a80c29d31d0a344c8597c6287d
matheusgoislimasilva@gmail.com <matheusgoislimasilva@gmail.com>
2024-12-27 01:49:29 -0300

    feat(Tests Base): Add Tests In Base ➕

---
+++ Example/Tests/Base/FeatureBaseTests.swift
@@ -0,0 +1,45 @@
+//
+//  FeatureBaseTests.swift
+//  DebugSwift
+//
+//  Created by Matheus Gois on 27/12/2024.
+//
+
+import XCTest
+@testable import DebugSwift
+
+class FeatureBaseTests: XCTestCase {
+
+    func testDebugSwiftFeature_allCases() {
+        // Given
+        let expectedCases: [DebugSwiftFeature] = [.network, .performance, .interface, .resources, .app]
+
+        // When
+        let allCases = DebugSwiftFeature.allCases
+
+        // Then
+        XCTAssertEqual(allCases, expectedCases, "DebugSwiftFeature.allCases should return all defined cases")
+    }
+
+    func testDebugSwiftSwizzleFeature_allCases() {
+        // Given
+        let expectedCases: [DebugSwiftSwizzleFeature] = [.network, .location, .views, .crashManager, .leaksDetector, .console]
+
+        // When
+        let allCases = DebugSwiftSwizzleFeature.allCases
+
+        // Then
+        XCTAssertEqual(allCases, expectedCases, "DebugSwiftSwizzleFeature.allCases should return all defined cases")
+    }
+
+    func testDebugSwiftFeatures_deprecated() {
+        // Given
+        let expectedType: DebugSwiftFeature.Type = DebugSwiftFeature.self
+
+        // When
+        let aliasType: DebugSwiftFeatures.Type = DebugSwiftFeatures.self
+
+        // Then
+        XCTAssertTrue(aliasType == expectedType, "DebugSwiftFeatures should be an alias for DebugSwiftFeature")
+    }
+}
