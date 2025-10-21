f9e1caa77b03e9e0a8d46083ce9a104f9d482c75
Baran Karaoğuz <b.b.karaoguz@gmail.com>
2025-10-21 16:28:25 +0300

    feat: downgrade swift version v6 -> v5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.0
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
@@ -23,6 +23,5 @@ let package = Package(
                 .process("Resources")
             ]
         )
-    ],
-    swiftLanguageModes: [.v6]
+    ]
 )
